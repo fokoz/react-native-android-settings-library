f7e58c1e9bf88c443722ca7d3831f84064dca409
fokoz <oatto.sp@gmail.com>
2017-10-21 01:18:17 +0700

    import componentname

--- android/src/main/java/com/reactlibrary/androidsettings/RNANAndroidSettingsLibraryModule.java
+++ android/src/main/java/com/reactlibrary/androidsettings/RNANAndroidSettingsLibraryModule.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 
+import android.content.ComponentName;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
