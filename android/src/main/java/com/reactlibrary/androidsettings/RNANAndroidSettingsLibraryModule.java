9be4fa6a3b4456d4024069d05c43c594b15d5452
fokoz <oatto.sp@gmail.com>
2017-11-23 12:11:41 +0700

    add huawei

--- android/src/main/java/com/reactlibrary/androidsettings/RNANAndroidSettingsLibraryModule.java
+++ android/src/main/java/com/reactlibrary/androidsettings/RNANAndroidSettingsLibraryModule.java
@@ -88,7 +88,10 @@ public class RNANAndroidSettingsLibraryModule extends ReactContextBaseJavaModule
                         intentCl.setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"));
                     } else if ("oneplus".equalsIgnoreCase(manufacturer)) {
                         intentCl.setComponent(new ComponentName("com.oneplus.security", "com.oneplus.security.chainlaunch.view.ChainLaunchAppListActivity"));
+                    } else if ("huawei".equalsIgnoreCase(manufacturer)) {
+                        intent.setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity"));
                     }
+  
 
                     // if  (list.size() > 0) {
                     //     context.startActivity(intent);
